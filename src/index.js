321ce1622d759222aa5a45bb6eaab2eb5450277a
Peter Bengtsson <mail@peterbe.com>
2018-11-04 07:19:29 -0500

    toast on service worker

--- src/index.js
+++ src/index.js
@@ -2,13 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
+import { toast } from "bulma-toast";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register({
-  onUpdate: () => {
-    // window.location.reload(true);
+  onUpdate: registration => {
+    showToast(() => {
+      console.warn("RELOADING NOW!");
+      registration.waiting.postMessage("skipWaiting");
+      window.location.reload(true);
+    });
   }
 });
+
+// setTimeout(() => {
+//   showToast(() => {
+//     console.warn("RELOAD NOW!");
+//   });
+// }, 2000);
+
+function showToast(callback) {
+  const a = document.createElement("a");
+  a.onclick = function(event) {
+    event.preventDefault();
+    callback();
+  };
+  a.text = "New version available. Click to reload.";
+  a.href = "/";
+  toast({
+    message: a,
+    type: "is-info",
+    dismissible: false,
+    closeOnClick: true,
+    duration: 10000
+  });
+}

9855c389be820edaa84626265e1ae2b9d36ea1bb
Peter Bengtsson <mail@peterbe.com>
2020-03-09 22:18:11 -0400

    build better

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-echo "Check if it's a dirty branch..."
-git diff-files --quiet || (echo "Dirty branch!" && git status && exit 1)
+#echo "Check if it's a dirty branch..."
+#git diff-files --quiet || (echo "Dirty branch!" && git status && exit 1)
 
 echo "Build the build..."
 ./bin/build.sh
 
-echo "Committing this latest build..."
-git add build.zip
-git commit -m "latest build (`date`)"
-git push origin master
+#echo "Committing this latest build..."
+#git add build.zip
+#git commit -m "latest build (`date`)"
+#git push origin master
+scp build.zip django@192.34.57.223:/home/django/chiveproxy/
 
 echo "Actually deploying..."
 UpgradeChiveproxy.sh

c965a38aec2b202ae24db3b46e3dba70a87f7dbb
Peter Bengtsson <mail@peterbe.com>
2020-10-15 17:20:06 -0400

    new ssh host

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,7 +12,7 @@ echo "Build the build..."
 #git add build.zip
 #git commit -m "latest build (`date`)"
 #git push origin master
-scp build.zip django@192.34.57.223:/home/django/chiveproxy/
+scp build.zip django@68.183.151.74:/home/django/chiveproxy/
 
 echo "Actually deploying..."
 UpgradeChiveproxy.sh
